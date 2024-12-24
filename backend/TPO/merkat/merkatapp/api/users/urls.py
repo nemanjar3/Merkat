9413a7d3769be1e8afa080a76936c33fe8fc0b52
smakivic <smakivic03@gmail.com>
2024-12-24 01:12:53 +0100

    added login token to backend

--- backend/TPO/merkat/merkatapp/api/users/urls.py
+++ backend/TPO/merkat/merkatapp/api/users/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from .create import UserCreateAPI
 from .login import LoginAPI
 from .profile import ProfileAPI
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('create/', UserCreateAPI.as_view(), name='user-create'),
     path('login/', LoginAPI.as_view(), name='user-login'),
     path('api/users/<int:user_id>/profile/', ProfileAPI.as_view(), name='profile'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
