80f5c8aae9e2231f526bc569741a33f4238f4879
smakivic <smakivic03@gmail.com>
2024-12-24 01:16:32 +0100

    added auth service to front

---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private tokenKey = 'auth_token';
+  private userKey = 'user';
+  public isLoggedIn$ = new BehaviorSubject<boolean>(false);
+
+  constructor(private http: HttpClient) {
+    // Check if the user is logged in when service is initialized
+    const token = localStorage.getItem(this.tokenKey);
+    this.isLoggedIn$.next(!!token);
+  }
+
+  login(username: string, password: string) {
+    return this.http.post('http://127.0.0.1:8000/api/users/login', { username, password });
+  }
+
+  saveUser(data: any) {
+    localStorage.setItem(this.tokenKey, data.tokens.access);
+    localStorage.setItem(this.userKey, JSON.stringify(data.username));
+    this.isLoggedIn$.next(true);
+  }
+
+  logout() {
+    localStorage.removeItem(this.tokenKey);
+    localStorage.removeItem(this.userKey);
+    this.isLoggedIn$.next(false);
+  }
+
+  getUser() {
+    return JSON.parse(localStorage.getItem(this.userKey) || '{}');
+  }
+}
