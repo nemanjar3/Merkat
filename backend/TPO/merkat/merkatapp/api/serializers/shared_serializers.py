0a7db57704c3c6ed6f5ad2a21c75cede92102ba5
PastMatter <mvuckovic2003@gmail.com>
2025-01-09 18:28:11 +0100

    listing by ID, added category and subcategory to listings API

--- backend/TPO/merkat/merkatapp/api/serializers/shared_serializers.py
+++ backend/TPO/merkat/merkatapp/api/serializers/shared_serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 # from ...models import User
 from ...models import User, Listing, Store, ListingAttributeValue, CategoryAttributes, SubCategoryAttributes
 # from .listing_serializers import ListingSerializer
+from ...models import Listing, Category, SubCategory
 
 
 class ListingAttributeValueSerializer(serializers.ModelSerializer):
@@ -22,9 +23,31 @@ class ListingAttributeValueSerializer(serializers.ModelSerializer):
 # prije bilo u listing serializeru
 from .shared_serializers import ListingAttributeValueSerializer
 
-class ListingSerializer(serializers.ModelSerializer):
+"""class ListingSerializer(serializers.ModelSerializer):
     attributes = ListingAttributeValueSerializer(many=True, source='attribute_values')  
 
     class Meta:
         model = Listing
-        fields = ['listing_id', 'title', 'description', 'price', 'posted_date', 'status', 'location', 'attributes']
+        fields = ['listing_id', 'title', 'description', 'price', 'posted_date', 'status', 'location', 'attributes']"""
+class CategorySerializerNew(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['category_id', 'category_name']
+
+class SubCategorySerializerNew(serializers.ModelSerializer):
+    class Meta:
+        model = SubCategory
+        fields = ['subcategory_id', 'subcategory_name']
+
+class ListingSerializer(serializers.ModelSerializer):
+    category = CategorySerializerNew()  
+    subcategory = SubCategorySerializerNew() 
+    attributes = ListingAttributeValueSerializer(many=True, source='attribute_values')  
+
+    class Meta:
+        model = Listing
+        fields = [
+            'listing_id', 'category', 'subcategory', 'attributes', 'user', 'title',  # dodaocu usera jos nekaga
+            'description', 'price', 'posted_date', 'updated_date', 
+            'status', 'location'
+        ]
