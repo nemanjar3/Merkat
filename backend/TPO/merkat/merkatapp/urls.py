8330c205c007ca16da1ddfc0c94bde3413fc0d5f
smakivic <smakivic03@gmail.com>
2025-01-14 20:34:45 +0100

    fix urls nadam se

--- backend/TPO/merkat/merkatapp/urls.py
+++ backend/TPO/merkat/merkatapp/urls.py
@@ -3,10 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    # This is 1st check for DataBase and DJANGO-ADMIN and ADD LISTING
-    path('create-profile/', views.create_profile, name='create_profile'),
-    path('success/', views.success, name='success'),
-
     path('', views.success, name='home'),  # Add this line for the root URL
-
 ]
